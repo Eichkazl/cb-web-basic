f8618d659a16c6ea60ccfff0087d975c8a5cec13
Eichkazl <85480803+Eichkazl@users.noreply.github.com>
2021-06-21 15:15:32 +0200

    21.06 15:15

--- ZooUndSeineBesucher/javaScript.js
+++ ZooUndSeineBesucher/javaScript.js
@@ -2,7 +2,7 @@ window.onload = function(){
   showSlides(1);
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -16,15 +16,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
