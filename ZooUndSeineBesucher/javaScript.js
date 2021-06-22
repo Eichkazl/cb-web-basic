c7caaff11d21e3590fa129509d4585b76379ea49
Eichkazl <85480803+Eichkazl@users.noreply.github.com>
2021-06-22 15:25:26 +0200

    22.06 15:25

--- ZooUndSeineBesucher/javaScript.js
+++ ZooUndSeineBesucher/javaScript.js
@@ -5,12 +5,10 @@ window.onload = function(){
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
