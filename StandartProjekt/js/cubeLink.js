73731d035d4d0f7804a75d1bdf1e5c281c7d5d8b
Eichkazl <85480803+Eichkazl@users.noreply.github.com>
2021-06-30 09:07:09 +0200

    30.6 09:06

--- StandartProjekt/js/cubeLink.js
+++ StandartProjekt/js/cubeLink.js
@@ -1,9 +1,29 @@
-const cube = document.querySelector(".cube");
+let cube = document.querySelector(".cube");
 
 cube.addEventListener("click", () => {
     if (window.location.pathname == "/StandartProjekt/html/cube.html") {
-        window.location.href = "seite3.html";
+        // window.location.href = "seite3.html";
     } else {
         window.location.href = "cube.html";
     }
 });
+ 
+let cube2 = document.querySelector(".cube2");
+
+  cube2.onclick = toggleAnimation;
+  cube.onclick = toggleAnimation;
+
+  cube2.style.webkitAnimationPlayState = 'running';
+  cube.style.webkitAnimationPlayState = 'running';
+
+function toggleAnimation() {
+
+  let style = this.style;
+
+  
+    if (style.webkitAnimationPlayState === 'running') {
+      style.webkitAnimationPlayState = 'paused';
+    } else {
+      style.webkitAnimationPlayState = 'running';
+  }
+}
