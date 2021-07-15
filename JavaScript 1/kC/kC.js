fafd010ab00491c671cc142a3679f4b9bb322bd1
Eichkazl <85480803+Eichkazl@users.noreply.github.com>
2021-07-15 15:28:13 +0200

    15.07

---
+++ JavaScript 1/kC/kC.js
@@ -0,0 +1,13 @@
+$('#button1').click(function (){
+
+// // Exercise 1
+//     $("input[value='Red']").next().text("Red");
+
+// // Exercise 2
+//     $("input[name!='color']").next().append(" color");
+
+// Exercise 3
+    $("input[name^='P']").text("Starts with P");
+});
+
+     	
